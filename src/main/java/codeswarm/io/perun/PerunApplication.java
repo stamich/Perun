47c9c3c31a2f3168e2db71315c11bcc3f4ea589f
Michal Stawarski <m.ski@poczta.fm>
2020-08-19 21:00:25 +0200

    init commit

---
+++ src/main/java/codeswarm/io/perun/PerunApplication.java
@@ -0,0 +1,13 @@
+package codeswarm.io.perun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codeswarm/io/perun/PerunApplicationTests.java
@@ -0,0 +1,13 @@
+package codeswarm.io.perun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
